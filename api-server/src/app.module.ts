297db112ed014789153a96fcd5207847bffcf63b
PatrickB1905 <patrickbutler2242@gmail.com>
2025-05-03 20:05:16 -0400

    feat(api-server): add NestJS GraphQL setup with HelloResolver and Dockerfile

---
+++ api-server/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+import { HelloResolver } from './hello.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+      sortSchema: true,
+    }),
+  ],
+  providers: [HelloResolver],
+})
+export class AppModule {}
